0ceb75077be18a546778f279ac24c6ece2376c81
NikitaBinary <nikita4office@gmail.com>
2024-01-15 13:35:19 +0530

    check

--- src/helper/generateToken.js
+++ src/helper/generateToken.js
@@ -5,6 +5,7 @@ async function generateToken(payload) {
     return jwt.sign(payload, process.env.JWT_SECRET_KEY)
 }
 
+
 async function verifyToken(token) {
     return jwt.verify(token, process.env.JWT_SECRET_KEY)
 }
