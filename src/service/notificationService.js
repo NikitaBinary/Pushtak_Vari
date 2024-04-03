9d55889811ce28b9a7ddd08c9496c0a32144a845
NikitaBinary <nikita4office@gmail.com>
2024-04-03 15:53:51 +0530

    add notification

--- src/service/notificationService.js
+++ src/service/notificationService.js
@@ -1,7 +1,9 @@
 const notification = require("../model/notificationModel")
 const userType = require("../model/userTypeModel")
 const notificationType = require("../model/notificationTypeModel")
-
+const { sendPushNotification } = require("../middleware/notification");
+const mongoose = require("mongoose")
+const user = require("../model/userModel")
 
 class AuthService {
     async createNotificationService(data) {
@@ -9,6 +11,50 @@ class AuthService {
             const userTypeData = await userType.findById({ _id: data.userType }, { _id: 1, userType: 1 })
             data.userType = userTypeData
             const notificationInfo = await notification.create(data);
+
+            if (notificationInfo.userType.userType == 'Users') {
+                const regularUserId = await user.find({ userType: 'REGULAR_USER' }, { _id: 1 })
+                const notificationObj = {
+                    body: {
+                        title: notificationInfo.notificationTitle,
+                        type: notificationInfo.notificationType,
+                        message: notificationInfo.message,
+                        usertype: "all",
+                        // uservalues: [new mongoose.Types.ObjectId("65d328e5d9d9a09ad6444ee7"), new mongoose.Types.ObjectId("660518c550caed9eedb0b8bd")]
+                        uservalues: regularUserId
+
+                    },
+                };
+                sendPushNotification(notificationObj).then(() => { }).catch(() => { });
+            }
+            if (notificationInfo.userType.userType == 'Institutes') {
+                const instituteUserId = await user.find({ userType: 'INSTITUTE_USER' }, { _id: 1 })
+                const notificationObj = {
+                    body: {
+                        title: notificationInfo.notificationTitle,
+                        type: notificationInfo.notificationType,
+                        message: notificationInfo.message,
+                        usertype: "all",
+                        uservalues: instituteUserId
+
+                    },
+                };
+                sendPushNotification(notificationObj).then(() => { }).catch(() => { });
+            }
+            if (notificationInfo.userType.userType == 'All') {
+                const allUserId = await user.find({}, { _id: 1 })
+                const notificationObj = {
+                    body: {
+                        title: notificationInfo.notificationTitle,
+                        type: notificationInfo.notificationType,
+                        message: notificationInfo.message,
+                        usertype: "all",
+                        uservalues: allUserId
+
+                    },
+                };
+                sendPushNotification(notificationObj).then(() => { }).catch(() => { });
+            }
             return notificationInfo
         } catch (error) {
             throw error;
