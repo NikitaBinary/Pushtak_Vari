eb87909b30d7b0994e84b996b6041b53766a2724
NikitaBinary <nikita4office@gmail.com>
2024-05-07 12:01:15 +0530

    notify

--- src/middleware/notification.js
+++ src/middleware/notification.js
@@ -8,38 +8,38 @@ const fcmServerKey = process.env.Server_Key; // put your server key here
 
 
 
-exports.sendNotification = async (registrationToken, data) => {
-    try {
-        const response = await axios.post(
-            'https://fcm.googleapis.com/fcm/send',
-            {
-                "to": registrationToken,
-                "notification": {
-                    "title": data.title,
-                    "body": "Rich Notification testing (body)",
-                    "mutable_content": true,
-                    "sound": "Tri-tone",
-                    "image": "https://via.placeholder.com/500x500.png?text=Sample+Image",
-                },
+// exports.sendNotification = async (registrationToken, data) => {
+//     try {
+//         const response = await axios.post(
+//             'https://fcm.googleapis.com/fcm/send',
+//             {
+//                 "to": registrationToken,
+//                 "notification": {
+//                     "title": data.title,
+//                     "body": "Rich Notification testing (body)",
+//                     "mutable_content": true,
+//                     "sound": "Tri-tone",
+//                     "image": "https://via.placeholder.com/500x500.png?text=Sample+Image",
+//                 },
 
-                "data": {
-                    // "url": "https://via.placeholder.com/500x500.png?text=Sample+Image",
-                    // "dl": "<deeplink action on tap of notification>"
-                }
-            },
+//                 "data": {
+//                     // "url": "https://via.placeholder.com/500x500.png?text=Sample+Image",
+//                     // "dl": "<deeplink action on tap of notification>"
+//                 }
+//             },
 
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `key=${fcmServerKey}`,
-                },
-            }
-        );
-        console.log('Notification sent successfully:', response.data);
-    } catch (error) {
-        console.error('Error sending notification:', error.response.data);
-    }
-}
+//             {
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     Authorization: `key=${fcmServerKey}`,
+//                 },
+//             }
+//         );
+//         console.log('Notification sent successfully:', response.data);
+//     } catch (error) {
+//         console.error('Error sending notification:', error.response.data);
+//     }
+// }
 exports.sendPushNotification = async (req) => {
     try {
         console.log("sendPushNotificationData body ");
